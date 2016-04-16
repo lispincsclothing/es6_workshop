925fad0e8cf5c38a44f1709f3265c8f1a0ba141a
[A <scullingnull+lisp@gmail.com>
2016-04-16 11:00:31 -0700

    First solution - shows problem with loop and scoping

--- exercises/ex1/ex1.js
+++ exercises/ex1/ex1.js
@@ -1,10 +1,14 @@
 var x = 2, fns = [];
+// are there any questions that I can remove ife, use block scoping or let
 
 (function(){
 	var x = 5;
 
 	for (var i=0; i<x; i++) {
 		// ..
+		fns[i]= function () {
+			return i;
+		}
 	}
 })();
 
