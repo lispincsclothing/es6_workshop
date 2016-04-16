ab08ebadbc94ab2710328062631e5d1ed821e46d
[A <scullingnull+lisp@gmail.com>
2016-04-16 11:03:12 -0700

    Used block pattern to remove need for ife

--- exercises/ex1/ex1.js
+++ exercises/ex1/ex1.js
@@ -1,16 +1,27 @@
 var x = 2, fns = [];
-// are there any questions that I can remove ife, use block scoping or let
 
-(function(){
-	var x = 5;
+// are there any ways that I can remove ife, use block scoping or let
 
+// (function(){
+// 	var x = 5;
+//
+// 	for (let i=0; i<x; i++) {
+// 		// ..
+// 		fns[i]= function () {
+// 			return i;
+// 		}
+// 	}
+// })();
+
+{let x = 5;
 	for (let i=0; i<x; i++) {
 		// ..
 		fns[i]= function () {
 			return i;
 		}
 	}
-})();
+}
+
 
 console.log(
 	(x * 2) === fns[x*2]()
