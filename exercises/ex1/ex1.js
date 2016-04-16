521f00aa77a89e93209b58d71cca482778ca988f
[A <scullingnull+lisp@gmail.com>
2016-04-16 11:10:56 -0700

    Notes about garbage collection with const, and single line assigment

--- exercises/ex1/ex1.js
+++ exercises/ex1/ex1.js
@@ -1,5 +1,10 @@
 const X = 2
 var fns = [];
+// Two notes - const has to has assignment, everything on line if declared is const....e.g..
+// const X=2, fns[];
+// Also, no way to garage collect with large array, e.g.
+const y = [....];
+y = null //doesn't work, since const can't be released
 
 // are there any ways that I can remove immeditely invoked function, use block scoping or let
 // http://benalman.com/news/2010/11/immediately-invoked-function-expression/
